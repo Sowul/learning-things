a60c21142414e9d7772f3b89d2a3b206740f3725
Sowul <bartosz.sowul@pm.me>
2024-09-12 13:19:19 +0200

    feat: init project with deps
    pywebview, python-fasthtml, schedule

---
+++ test.py
@@ -0,0 +1,20 @@
+import webview
+
+html = """
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+        <title>Test app</title>
+	</head>
+	<body style = "background-color: transparent;">
+       Test
+	</body>
+</html>
+"""
+
+if __name__ == '__main__':
+    # Create a resizable webview window with minimum size constraints
+    window = webview.create_window('Transparent window', html=html, transparent=True, frameless=True, easy_drag=True)
+    window.on_top = True
+    webview.start()
